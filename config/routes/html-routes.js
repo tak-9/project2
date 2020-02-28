a868ff16a4f77c49ea075a9516b23bfb2d679748
Dinatolemy <dina.a.tolemy@gmail.com>
2020-02-28 19:13:04 +1100

    edit routes

--- config/routes/html-routes.js
+++ config/routes/html-routes.js
@@ -4,31 +4,51 @@ var path = require("path");
 //  we need to change the pages name to the right one 
 /////
 ////
-module.exports = function(app) {
-      // home pages (login page)
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-      });
-
-      app.get("/staffmenu", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/staff.html"));
-      });
-
-      app.get("/studentmenu", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/student.html"));
-      });
+module.exports = function (app) {
+  // home pages (login page)
+  app.get("/", function (req, res) {
+    console.log("app.get / req ", req.User);
+      if (req.User.userType === "student") {
+        res.redirect("/student");
+      } else if (req.User.userType === "staff") {
+        res.redirect("/staff");
       
-      app.get("/signup", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/signup.html"));
-      });
-      // grades for students by staff only 
-      app.get("/studentgrades", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/grades.html"));
-      });
-      //the grade page for one student
-      app.get("/grade", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/grade.html"));
-      });
+    } else {
+      res.sendFile(path.join(__dirname, "../public/home.html"));
+    }
+  });
+
+  app.get("/login", function(req, res) {
+    if (req.User.userType === "student") {
+      res.redirect("/student");
+    } else if (req.User.userType === "staff") {
+      res.redirect("/staff");
+    } else {
+      res.sendFile(path.join(__dirname, "../public/login.html"));
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+
+  });
+
+  app.get("/staff", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/staff.html"));
+  });
+
+  app.get("/student", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/student.html"));
+  });
+
+  app.get("/signup", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+  // grades for students by staff only 
+  app.get("/studentgrades", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/grades.html"));
+  });
+  //the grade page for one student
+  app.get("/grade", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/grade.html"));
+  });
 
 
 };
--- config/routes/user-api-routes.js
+++ config/routes/user-api-routes.js
@@ -1,27 +1,39 @@
 var db = require("../models");
 //routes
-module.exports = function(app) {
+module.exports = function (app) {
 
-    //post method for the sign in ..
+  //post method for the sign in ..
 
-//post route for sign up
+  //post route for sign up
 
-    app.post("/api/signup", function(req, res) {
-        db.User.create(req.body).then(function(dbSign) {
-          res.json(dbSign);
-        });
+  app.post("/api/signup", function (req, res) {
+    db.User.create(req.body).then(function (dbSign) {
+
+      res.json(dbSign);
+      res.redirect(307, "/api/login");
+    })
+      .catch(function (err) {
+        res.status(401).json(err);
       });
+  });
 
-      //put route for the student to update his details 
-      app.put("/api/signup", function(req, res) {
-        db.User.update(
-          req.body,
-          {
-            where: {
-              id: req.body.id
-            }
-          }).then(function(dbsign) {
-          res.json(dbsign);
-        });
+  //put route for the student to update his details 
+  app.put("/api/signup", function (req, res) {
+    db.User.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function (dbsign) {
+        res.json(dbsign);
+      }).catch(function (err) {
+        res.status(401).json(err);
       });
+  });
+
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 };
