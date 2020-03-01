431c5a9f1688f8a2008356c2a661863a699d4181
Takuji Okubo <takuji.okubo@gmail.com>
2020-03-01 15:41:48 +1100

    use await to improve readability

--- db/seed.js
+++ db/seed.js
@@ -1,56 +1,55 @@
 var db = require("../models");
 
-createUsers();
-
-function createUsers(){ 
 // Delete all from user, then insert dummy data.
-db.User.destroy({ where: {} })
-    .then(() => {
-        db.User.create({
-            userFirstName: "FirstTeacher",
-            userLastName: "LastTeacher",
-            //    userName: "teacher",
-            password: "123",
-            street: "teacher st",
-            suburb: "teacher suburb",
-            school: "teacher's school",
-            birthdate: "date",
-            phoneNumber: "1234567890",
-            email: "teacher@test.com",
-            yeargroup: "1",
-            userType: "staff"
-        }) 
-        .then(() => {
-            db.User.create({
-                userFirstName: "FirstStudent",
-                userLastName: "LastStudent",
-                //    userName: "Student",
-                password: "123",
-                street: "Student st",
-                suburb: "Student suburb",
-                school: "Student's school",
-                birthdate: "date",
-                phoneNumber: "0237890",
-                email: "student@test.com",
-                yeargroup: "1",
-                userType: "student"
-            })
-            .then(()=>{
-                createParent();
-            })
-        }) 
-    }) 
-}; //end createUsers()
+cleanUpDb();
 
+async function cleanUpDb(){
+    await db.User.destroy({ where: {} })
+    await db.Parent.destroy({ where: {} })
+    createUsers();
+}
 
-function createParent(){
+async function createUsers() {
+    await db.User.create({
+        userFirstName: "FirstTeacher",
+        userLastName: "LastTeacher",
+        //    userName: "teacher",
+        password: "123",
+        street: "teacher st",
+        suburb: "teacher suburb",
+        school: "teacher's school",
+        birthdate: "date",
+        phoneNumber: "1234567890",
+        email: "teacher@test.com",
+        yeargroup: "1",
+        userType: "staff"
+    })
+
+    await db.User.create({
+        userFirstName: "FirstStudent",
+        userLastName: "LastStudent",
+        //    userName: "Student",
+        password: "123",
+        street: "Student st",
+        suburb: "Student suburb",
+        school: "Student's school",
+        birthdate: "date",
+        phoneNumber: "0237890",
+        email: "student@test.com",
+        yeargroup: "1",
+        userType: "student"
+    })
+    createParent();
+} 
+
+async function createParent(){
     db.User.findOne({
         where: {
             email: "student@test.com"
         }
     })
     .then((data)=>{
-        console.log(data);
+        //console.log(data);
         db.Parent.create({
             firstName: "firstParent",
             lastName: "lastParent",
