e679f551b7c38fcf2863c8a72eca304172123c9e
Takuji Okubo <takuji.okubo@gmail.com>
2020-03-02 18:11:13 +1100

    for handlebars

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/grades-api-routes.js")(app);
