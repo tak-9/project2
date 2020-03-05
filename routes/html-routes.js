38a85062c6e34c365aee146fa19e0e2787fe6dc3
Dinatolemy <dina.a.tolemy@gmail.com>
2020-03-05 17:59:00 +1100

    gradesfunction

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -42,18 +42,18 @@ module.exports = function (app) {
   });
 
   app.get("/staff/view_student_list", isStaff, function (req, res) {
-    db.User.findAll({
-      include: [db.Parent],
-      where: { userType: "student" }
-    })
-      .then((dbResult) => {
-        var dbResult = JSON.parse(JSON.stringify(dbResult));
-        var hbsObject = { "students": dbResult };
+    sqlStr =
+    "select u.id as id, u.name as name, address, school, bithdate, u.phone as phone, u.email as email, yeargroup, userType, "+
+    "p.name as parentname, p.phone as parentphone, p.email as parentemail "+
+    "from users u " + 
+    "join parents p on u.ParentId = p.id"
+    
+    db.sequelize.query(sqlStr)
+    .then((dbResult)=>{
+        console.log(dbResult[0]);
+        var hbsObject  = { "students" : dbResult[0] };
         res.render("view_student_list", hbsObject);
-      })
-      .catch((err) => {
-        console.log("err", err);
-      })
+    })
   });
 
   app.get("/staff/view_quiz_result", isStaff, function (req, res) {
@@ -84,7 +84,8 @@ module.exports = function (app) {
       "p.name as parentname, p.phone as parentphone, p.email as parenetemail " +
       "from users u " +
       "inner join parents p on u.ParentId = p.id";
-    db.sequelize.query(sqlStr).then(function (dbUserDetails) {
+    db.sequelize.query(sqlStr)
+    .then(function (dbUserDetails) {
       console.log("this the user details ", dbUserDetails);
       // var dbUserDetails = JSON.parse(JSON.stringify(dbUserDetails));
       var userdetail = dbUserDetails[0].filter(x => x.userId === myId);
@@ -95,19 +96,18 @@ module.exports = function (app) {
   });
 
   app.get("/student/view_student_grade", isStudent, function (req, res) {
-    // Get id of currently logged in student.
     console.log("app.get / req.user", req.user);
-    var myId=req.user.id;
-    var gradeSqlString="SELECT enrolments.UserId,grade,courseName,Homework.name FROM toutoring_center.grades join homework join courses join enrolments on grades.UserId=enrolments.userId;";
-    db.sequelize.query(gradeSqlString).then(function(gradeDetails){
-      console.log(gradeDetails);
-      var userGrades=gradeDetails[0].filter(x=> x.userId === myId);
-      console.log("my grades are : ",userGrades);
-      var userGrades=JSON.parse(JSON.stringify(userGrades));
-      var gradesObj={"students":userGrades[0]};
-      res.render("view_student_grade", gradesObj);
-    })
- .catch(function (err) {
+   var myId=req.user.id;
+   var sqlString="SELECT Grades.UserId,grade,courseName,Homework.name FROM toutoring_center.grades join homework join courses join enrolments on grades.UserId=enrolments.userId;";
+   db.sequelize.query(sqlString)
+  .then(function (dbUserGrades) {
+      console.log("this the user Grades ", dbUserGrades[0]);
+      var UserGrades = dbUserGrades[0].filter(x => x.UserId === myId);
+      console.log("my user grades are : --",UserGrades[0]);
+      var UserGrades = JSON.parse(JSON.stringify(UserGrades));
+      var hbsObject = { "students": UserGrades[0] };
+      res.render("view_student_grade", hbsObject[0]);
+    }).catch(function (err) {
       // handle error;
       console.log(err)
     });
